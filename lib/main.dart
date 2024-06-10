e91fa2834e5b213b0eb40f09883b79214aa1d208
Adam Amzar <adamamzar98@gmail.com>
2024-06-11 01:16:09 +0800

    11 June 2024 Off White wallpaper

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFFFaf9f6),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
